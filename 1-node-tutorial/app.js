9706a72c871dad6421e5bb7eb274adb0dd4a65a6
john-smilga <johnsmilga@gmail.com>
2021-02-11 12:24:37 -0800

    a bit more testing

--- 1-node-tutorial/app.js
+++ 1-node-tutorial/app.js
@@ -1,16 +1,1 @@
-var http = require('http')
-var fs = require('fs')
-
-http
-  .createServer(function (req, res) {
-    // const text = fs.readFileSync('./content/big.txt', 'utf8')
-    // res.end(text)
-    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
-    fileStream.on('open', () => {
-      fileStream.pipe(res)
-    })
-    fileStream.on('error', (err) => {
-      res.end(err)
-    })
-  })
-  .listen(5000)
+console.log('Welcome to Node Tutorial')
