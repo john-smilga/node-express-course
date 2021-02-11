a2ab1e42ce82ed8282dd283dc2565332298387b0
john-smilga <johnsmilga@gmail.com>
2021-02-11 12:17:07 -0800

    node tutorial complete

--- 1-node-tutorial/15-create-big-file.js
+++ 1-node-tutorial/15-create-big-file.js
@@ -1,4 +1,4 @@
 const { writeFileSync } = require('fs')
-for (let i = 0; i < 100000; i++) {
-  writeFileSync('./content/big.txt', `hello world ${i}\n`)
+for (let i = 0; i < 10000; i++) {
+  writeFileSync('./content/big.txt', `hello world ${i}\n`, { flag: 'a' })
 }
---
+++ 1-node-tutorial/16-streams.js
@@ -0,0 +1,13 @@
+const { createReadStream } = require('fs')
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+const stream = createReadStream('./content/big.txt')
+
+stream.on('data', (result) => {
+  console.log(result)
+})
+stream.on('error', (err) => console.log(err))
