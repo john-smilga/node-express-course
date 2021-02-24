e4dc20899f501ff59f8146c43a14cc04d21ac983
john-smilga <johnsmilga@gmail.com>
2021-02-24 14:10:23 -0800

    5 complete

--- 5-methods/setup/app.js
+++ 5-methods/setup/app.js
@@ -31,7 +31,36 @@ app.post('/api/people', (req, res) => {
   }
   res.status(201).json({ sucess: true, person: name })
 })
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params
 
+  const person = people.find((person) => person.id === Number(id))
+  if (!person) {
+    return res
+      .status(404)
+      .json({ sucess: false, msg: `no person with id :${id}` })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = req.body.name
+    }
+    return person
+  })
+  return res.status(200).json({ sucess: true, data: newPeople })
+})
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id))
+  if (!person) {
+    return res
+      .status(404)
+      .json({ sucess: false, msg: `no person with id :${req.params.id}` })
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  )
+
+  return res.status(200).json({ sucess: true, data: newPeople })
+})
 app.listen(5000, () => {
   console.log('server listening on port 5000...')
 })
