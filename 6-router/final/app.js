78dbca1fed3a7dc46ffca694f04e7178650ff6e5
john-smilga <johnsmilga@gmail.com>
2021-02-24 14:35:10 -0800

    6 express router complete

--- 6-router/final/app.js
+++ 6-router/final/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
+const jobs = require('./routes/jobs')
 
-app.get('/', (req, res) => {
-  res.send('Express Router Setup')
-})
+// set jobs router
+app.use('/api/jobs', jobs)
 
 app.listen(5000, () => {
   console.log('server listening on port 5000...')
