d075000cf662bc9487e1d6103ff99fb4795f11dd
john-smilga <johnsmilga@gmail.com>
2021-03-06 12:35:34 -0800

    Express Tutorial Complete

--- 2-express-tutorial/final/10-middleware-options.js
+++ 2-express-tutorial/final/10-middleware-options.js
@@ -1,34 +1,28 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const logger = require('./logger')
+const authorize = require('./authorize')
+//  req => middleware => res
 
-// req => middleware => res
-
-const logger = (req, res, next) => {
-  console.log(req)
-  const method = req.method
-  const url = req.url
-  const time = new Date()
-  console.log(method, url, time)
-  next()
-}
-const authorize = (req, res, next) => {
-  const { user } = req.query
-  if (user) {
-    req.user = user
-    next()
-  } else {
-    res.status(401).send('Unauthorized')
-  }
-}
-app.use([logger, authorize])
+// app.use([logger, authorize])
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
-  res.send(`Hello there ${req.user}`)
+  res.send('Home')
 })
 app.get('/about', (req, res) => {
   res.send('About')
 })
+app.get('/api/products', (req, res) => {
+  res.send('Products')
+})
+app.get('/api/items', (req, res) => {
+  console.log(req.user)
+  res.send('Items')
+})
 
 app.listen(5000, () => {
-  console.log('server listening on port 5000...')
+  console.log('Server is listening on port 5000....')
 })
--- 2-express-tutorial/public/browser-app.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle')
-const links = document.querySelector('.links')
-
-navToggle.addEventListener('click', function () {
-  links.classList.toggle('show-links')
-})
