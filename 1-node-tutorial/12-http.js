633383d2e40d29b2e6995ddb819ce6a579c371ec
John <johnsmilga@gmail.com>
2021-03-25 19:08:54 -0700

    IF,ELSE IF, ELSE BUG FIX

--- 1-node-tutorial/12-http.js
+++ 1-node-tutorial/12-http.js
@@ -1,17 +1,34 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+  //   if (req.url === '/') {
+  //     res.end('Welcome to our home page')
+  //   }
+  //   if (req.url === '/about') {
+  //     res.end('Here is our short history')
+  //   }
+  //   res.end(`
+  //   <h1>Oops!</h1>
+  // <p>We can't seem to find the page you are looking for</p>
+  // <a href="/">back home</a>
+  //   `)
+  // ###################################
+  // ###################################
+  //
+  //  IF YOU GET ERRORS WHILE USING ABOVE SETUP,
+  // SWITCH TO IF, ELSE IF, ELSE (BELOW)
+  // WE COVER THE CAUSE, LATER IN EXPRESS TUTORIAL
   if (req.url === '/') {
     res.end('Welcome to our home page')
-  }
-  if (req.url === '/about') {
+  } else if (req.url === '/about') {
     res.end('Here is our short history')
+  } else {
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>
+    `)
   }
-  res.end(`
-  <h1>Oops!</h1>
-<p>We can't seem to find the page you are looking for</p>
-<a href="/">back home</a>
-  `)
 })
 
 server.listen(5000)
