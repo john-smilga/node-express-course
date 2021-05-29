0af8de11a24e6aa32b0a1dc9d6e9e9924869da32
john-smilga <johnsmilga@gmail.com>
2021-05-29 13:59:17 -0700

    in 03-task-manager switched final to 500

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -21,7 +21,7 @@ app.use('/api/tasks', tasks)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
