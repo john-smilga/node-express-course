4cad47c5c34cd73a7ca78f763967f3640419b06f
john-smilga <johnsmilga@gmail.com>
2021-06-10 07:53:16 -0700

    03-switched back to local setup

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -6,12 +6,7 @@ require('dotenv').config()
 const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
-const helmet = require('helmet')
-const cors = require('cors')
-
 // middleware
-app.use(helmet())
-app.use(cors())
 
 app.use(express.static('./public'))
 app.use(express.json())
@@ -22,7 +17,7 @@ app.use('/api/v1/tasks', tasks)
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
