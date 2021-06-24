9182608ecea842efe2d4aee26cb9e882eb7767a6
Jisan <jishananam@iut-dhaka.edu>
2021-06-25 00:39:21 +0600

    Learning about public Folder

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,37 +1,12 @@
-const http=require('http')
-const { readFileSync} = require('fs')
-const homePage = readFileSync('./navbar-app/index.html')
-const styles=readFileSync('./navbar-app/styles.css')
-const logo = readFileSync('./navbar-app/logo.svg')
-const logic=readFileSync('./navbar-app/browser-app.js')
-
-const server=http.createServer((req, res) =>{
-    if(req.url==='/'){
-    console.log('No More Today')
-    res.end('<h1>My name </h1>')}
-    else if(req.url==='/about'){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write(homePage)
-        res.end()
-    }
-    else if (req.url === '/styles.css'){
-        res.writeHead(200,{'content-type':'text/css'})
-        res.write(styles)
-        res.end()
-    }
-    else if(req.url==='/logo.svg'){
-        res.writeHead(200, { 'content-type': 'image/svg+xml' })
-        res.write(logo)
-        res.end()
-    }
-    else if(req.url==='/browser-app.js'){
-        res.writeHead(200, { 'content-type': 'text/javascript' })
-        res.write(logic)
-        res.end()
-    }
-
-    else{
-        res.end('No NO ')
-    }
+const express=require('express')
+const app = express();
+const path = require('path');
+app.use(express.static('./public'));
+app.get('/',(req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'));
 })
-server.listen(5000)
+
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>Resource not found</h1>')
+})
+app.listen(5000)
