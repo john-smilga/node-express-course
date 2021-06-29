e70e56ace8e412101523a49b448f96cbaf52dbbe
Graham Allan <grahamallan31+git@gmail.com>
2021-06-29 09:49:37 +0100

    Added next to parameters of controllers using next

--- 03-task-manager/final/controllers/tasks.js
+++ 03-task-manager/final/controllers/tasks.js
@@ -20,7 +20,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
 
   res.status(200).json({ task })
 })
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params
   const task = await Task.findOneAndDelete({ _id: taskID })
   if (!task) {
@@ -28,7 +28,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   }
   res.status(200).json({ task })
 })
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params
 
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
