576659ed9d5d5d63a9d1694280526cc4fc031736
john-smilga <johnsmilga@gmail.com>
2021-07-21 21:25:04 -0700

    docs added to 06-jobs-api

--- 06-jobs-api/final/app.js
+++ 06-jobs-api/final/app.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 require('express-async-errors');
 
-const docs = require('./docs');
-const swaggerUI = require('swagger-ui-express');
 // extra security packages
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -25,8 +29,13 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
-app.use('/', swaggerUI.serve, swaggerUI.setup(docs));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
--- 06-jobs-api/final/middleware/authentication.js
+++ 06-jobs-api/final/middleware/authentication.js
@@ -1,27 +1,23 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const { UnauthenticatedError } = require('../errors');
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
 
 const auth = async (req, res, next) => {
   // check header
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization
   if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new UnauthenticatedError('Authentication invalid');
+    throw new UnauthenticatedError('Authentication invalid')
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(' ')[1]
 
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-
-    // const user = User.findById(payload.id).select('-password')
-    // req.user = user
-
-    req.user = { userId: payload.userId, name: payload.name };
-    next();
+    req.user = { userId: payload.userId, name: payload.name }
+    next()
   } catch (error) {
-    throw new UnauthenticatedError('Authentication invalid');
+    throw new UnauthenticatedError('Authentication invalid')
   }
-};
+}
 
-module.exports = auth;
+module.exports = auth
