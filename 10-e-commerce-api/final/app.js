0b3bf9bf569b20b8269fc23de585d3d5361aeb3d
john-smilga <johnsmilga@gmail.com>
2021-09-06 08:48:33 -0700

    change 10 e-commerce final to 5000

--- 10-e-commerce-api/final/app.js
+++ 10-e-commerce-api/final/app.js
@@ -50,7 +50,7 @@ app.use('/api/v1/orders', authenticateUser, orderRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
