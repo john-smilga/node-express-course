a369b14277a7d2be431a936b978fb128ac2e096c
john-smilga <johnsmilga@gmail.com>
2021-09-18 10:26:26 -0700

    06-jobs-api rate limiter update

--- 06-jobs-api/final/app.js
+++ 06-jobs-api/final/app.js
@@ -24,11 +24,17 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }));
 
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
