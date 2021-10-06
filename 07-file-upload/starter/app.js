35d6154a9ae81b7820b22cb0ba0c08781b1f5be7
john-smilga <johnsmilga@gmail.com>
2021-10-06 14:29:45 -0700

    fixed bug and updated readme

--- 07-file-upload/starter/app.js
+++ 07-file-upload/starter/app.js
@@ -23,6 +23,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
+
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
