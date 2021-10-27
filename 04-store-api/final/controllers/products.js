beca9d9b99cd340948e074a733d346942aeff6de
john-smilga <johnsmilga@gmail.com>
2021-10-27 15:41:25 -0700

    fixed small typo

--- 04-store-api/final/controllers/products.js
+++ 04-store-api/final/controllers/products.js
@@ -1,24 +1,24 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({ price: { $gt: 30 } })
     .sort('price')
-    .select('name price')
+    .select('name price');
 
-  res.status(200).json({ products, nbHits: products.length })
-}
+  res.status(200).json({ products, nbHits: products.length });
+};
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields, numericFilters } = req.query
-  const queryObject = {}
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
 
   if (featured) {
-    queryObject.featured = featured === 'true' ? true : false
+    queryObject.featured = featured === 'true' ? true : false;
   }
   if (company) {
-    queryObject.company = company
+    queryObject.company = company;
   }
   if (name) {
-    queryObject.name = { $regex: name, $options: 'i' }
+    queryObject.name = { $regex: name, $options: 'i' };
   }
   if (numericFilters) {
     const operatorMap = {
@@ -27,47 +27,47 @@ const getAllProducts = async (req, res) => {
       '=': '$eq',
       '<': '$lt',
       '<=': '$lte',
-    }
-    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
     let filters = numericFilters.replace(
       regEx,
       (match) => `-${operatorMap[match]}-`
-    )
-    const options = ['price', 'rating']
+    );
+    const options = ['price', 'rating'];
     filters = filters.split(',').forEach((item) => {
-      const [field, operator, value] = item.split('-')
+      const [field, operator, value] = item.split('-');
       if (options.includes(field)) {
-        queryObject[field] = { [operator]: Number(value) }
+        queryObject[field] = { [operator]: Number(value) };
       }
-    })
+    });
   }
 
-  let result = Product.find(queryObject)
+  let result = Product.find(queryObject);
   // sort
   if (sort) {
-    const sortList = sort.split(',').join(' ')
-    result = result.sort(sortList)
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
   } else {
-    result = result.sort('createAt')
+    result = result.sort('createdAt');
   }
 
   if (fields) {
-    const fieldsList = fields.split(',').join(' ')
-    result = result.select(fieldsList)
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
   }
-  const page = Number(req.query.page) || 1
-  const limit = Number(req.query.limit) || 10
-  const skip = (page - 1) * limit
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-  result = result.skip(skip).limit(limit)
+  result = result.skip(skip).limit(limit);
   // 23
   // 4 7 7 7 2
 
-  const products = await result
-  res.status(200).json({ products, nbHits: products.length })
-}
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 module.exports = {
   getAllProducts,
   getAllProductsStatic,
-}
+};
