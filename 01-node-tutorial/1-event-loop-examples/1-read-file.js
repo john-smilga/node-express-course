eb26a66c277e42c4eb11fb014b1108f288bc3eb7
Sonu Kumar <90151433+kumso@users.noreply.github.com>
2022-08-20 08:30:54 +0530

    Update 1-read-file.js
    We can use else block for a better understanding of code.

--- 01-node-tutorial/1-event-loop-examples/1-read-file.js
+++ 01-node-tutorial/1-event-loop-examples/1-read-file.js
@@ -5,9 +5,10 @@ console.log('started a first task')
 readFile('./content/first.txt', 'utf8', (err, result) => {
   if (err) {
     console.log(err)
-    return
   }
-  console.log(result)
-  console.log('completed first task')
+  else {
+    console.log(result)
+    console.log('completed first task')
+  }
 })
 console.log('starting next task')
