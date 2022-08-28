28340b04bc29cad86498fb349d7ae542e26342dc
john smilga <johnsmilga@gmail.com>
2022-08-28 14:20:44 -0700

    6.5-jobster-bug-fix

--- 06.5-jobster-api/final/controllers/jobs.js
+++ 06.5-jobster-api/final/controllers/jobs.js
@@ -137,17 +137,19 @@ const showStats = async (req, res) => {
     { $limit: 6 },
   ]);
 
-  monthlyApplications = monthlyApplications.map((item) => {
-    const {
-      _id: { year, month },
-      count,
-    } = item;
-    const date = moment()
-      .month(month - 1)
-      .year(year)
-      .format('MMM Y');
-    return { date, count };
-  });
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format('MMM Y');
+      return { date, count };
+    })
+    .reverse();
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
