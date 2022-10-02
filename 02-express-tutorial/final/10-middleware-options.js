4cbe58f9b0c3b3d00a82cb2f962616ed7e1a9f66
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-10-02 08:15:23 -0700

    Update 10-middleware-options.js
    Made some change for product dan people from data.js

--- 02-express-tutorial/final/10-middleware-options.js
+++ 02-express-tutorial/final/10-middleware-options.js
@@ -3,6 +3,8 @@ const app = express()
 const morgan = require('morgan')
 const logger = require('./logger')
 const authorize = require('./authorize')
+const { product } = require('./data #made product definition
+const { people } = require('./data') #made people definition
 //  req => middleware => res
 
 // app.use([logger, authorize])
@@ -15,12 +17,27 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('About')
 })
+#get product api
 app.get('/api/products', (req, res) => {
-  res.send('Products')
+  const newProducts = product.map((items)=>{
+    const {id, name, image} = items;
+    return {id, name, image}
+  })
+  res.json(newProducts)
 })
+#get people api
+app.get('/api/people', (req,res)=>{
+  const newPeople = people.map((items)=>{
+    const {id, name } = items;
+    return {id, name}
+  })
+  console.log(newPeople)
+  res.json(newPeople)
+})
+
 app.get('/api/items', (req, res) => {
   console.log(req.user)
-  res.send('Items')
+  res.send(''You insert john name'')
 })
 
 app.listen(5000, () => {
