4cc14b6b559a5ff4a83d41a0604eaad392e0479d
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-10-05 08:40:55 -0700

    Update 13-router-people.js
    Add some variable for it

--- 02-express-tutorial/final/13-router-people.js
+++ 02-express-tutorial/final/13-router-people.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const {
-  getPeople,
-  createPerson,
-  createPersonPostman,
-  updatePerson,
-  deletePerson,
+    getPeople, 
+    createPerson, 
+    createPersonPostman, 
+    findPersonId, #Add findPerson
+    updatePerson,
+    deletePerson
 } = require('../controllers/people')
 
 // router.get('/', getPeople)
@@ -15,8 +16,8 @@ const {
 // router.put('/:id', updatePerson)
 // router.delete('/:id', deletePerson)
 
-router.route('/').get(getPeople).post(createPerson)
-router.route('/postman').post(createPersonPostman)
-router.route('/:id').put(updatePerson).delete(deletePerson)
+router.route('/').get(getPeople).post(createPerson).put(findPersonId); #Add findPerson
+router.route('/postman').post(createPersonPostman);
+router.route('/:id').put(updatePerson).delete(deletePerson);
 
 module.exports = router
