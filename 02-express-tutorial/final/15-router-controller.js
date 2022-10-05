839dc8c4820118c6ef723eac064b2a297e54e15c
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-10-05 08:46:12 -0700

    Update 15-router-controller.js
    Add some change for findPersonId function

--- 02-express-tutorial/final/15-router-controller.js
+++ 02-express-tutorial/final/15-router-controller.js
@@ -1,66 +1,72 @@
 let { people } = require('../data')
 
-const getPeople = (req, res) => {
-  res.status(200).json({ success: true, data: people })
+
+const getPeople = (req,res) =>{
+    res.status(200).json({success:true, data:people})
 }
 
-const createPerson = (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'please provide name value' })
-  }
-  res.status(201).send({ success: true, person: name })
-}
-
-const createPersonPostman = (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'please provide name value' })
-  }
-  res.status(201).send({ success: true, data: [...people, name] })
-}
-
-const updatePerson = (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
-
-  const person = people.find((person) => person.id === Number(id))
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}` })
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name
+const createPerson = (req,res) =>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false, msg:'please provide name value'})
     }
-    return person
-  })
-  res.status(200).json({ success: true, data: newPeople })
+    res.status(201).send({success:true, person:name})
+    console.log(name)
 }
 
-const deletePerson = (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id))
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${req.params.id}` })
-  }
-  const newPeople = people.filter(
-    (person) => person.id !== Number(req.params.id)
-  )
-  return res.status(200).json({ success: true, data: newPeople })
+const createPersonPostman = (req,res)=>{
+    const {name} = req.body
+    const {id} = req.body
+    if(!name){
+        return res.status(400).json({success:false, msg:'please provide name value'})
+    }
+    res.status(201).send({success:true, data: [...people, name, id], msg:`You add new name to our datalist : ${name} `})
+}
+
+const findPersonId = (req,res)=>{
+    const {name} = req.body
+    
+    const person = people.find((person)=>person.name === name)
+    if(!person){
+        return res.status(404).json({success:false, msg:`No person with name ${name} in our data. Fill Name again`})
+    }
+    res.status(200).json({ success:true, data:name, msg:'Name matched'})
+    console.log(name);
+}
+
+const updatePerson = (req,res)=>{
+    const {id} = req.params
+    const {name} = req.body
+
+    const person = people.find((person)=>person.id === Number(id))
+    if(!person){
+        return res.status(404).json({success:false, msg:`No person with id ${id}`})
+    }
+    const newPeople = people.map((person)=>{
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({ success:true, data:newPeople })
+    console.log(id, name);
+}
+
+const deletePerson = (req,res)=>{
+    const person = people.find((person)=>person.id === Number(req.params.id))
+
+    if(!person){
+        return res.status(404).json({success:false, msg: `No person with id ${req.params.id}`})
+    }
+    const newPeople = people.filter((person)=>person.id !== Number(req.params.id));
+    return res.status(200).json({success:true, data:newPeople, msg: `Data with id ${req.params.id} deleted`}) //add message for deleted data
 }
 
 module.exports = {
-  getPeople,
-  createPerson,
-  createPersonPostman,
-  updatePerson,
-  deletePerson,
+    getPeople, 
+    createPerson, 
+    createPersonPostman, 
+    findPersonId,
+    updatePerson,
+    deletePerson
 }
