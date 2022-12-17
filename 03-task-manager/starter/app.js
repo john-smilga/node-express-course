e81fb5dcf8fdacde475be0111b8e1644be1cac0e
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-17 15:01:40 +0330

    create server

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Welcome to app')
+})
+
+app.listen(port, () => {
+  console.log('Listening for 5000 port')
+})
