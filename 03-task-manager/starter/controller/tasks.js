de5538c2a7b2f6e164b3060c490149efb6cd448c
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-17 16:56:03 +0330

    add ROutes in tasks route

--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -2,4 +2,17 @@ const getAllTasks = (req, res) => {
   res.send('TASKS')
 }
 
-module.exports = { getAllTasks }
+const createTask = (req, res) => {
+  res.send('Create tasks')
+}
+const getTask = (req, res) => {
+  res.send('Create tasks')
+}
+const updateTask = (req, res) => {
+  res.send('Create tasks')
+}
+const deleteTask = (req, res) => {
+  res.send('Create tasks')
+}
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask }
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,8 +1,15 @@
 const express = require('express')
-const { getAllTasks } = require('../controller/tasks')
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require('../controller/tasks')
 
 const router = express.Router()
 
-router.get('/', getAllTasks)
+router.get('/', getAllTasks).post('/', createTask)
+router.get('/:id', getTask).patch('/:id', updateTask).delete(':/id', deleteTask)
 
 module.exports = router
