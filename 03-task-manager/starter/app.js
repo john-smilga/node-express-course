325ec107c8f652db7d6eeeb867d17d99cbae40b3
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-20 12:34:27 +0000

    Creating Task Manager App

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+// Routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+
+
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
+
+//console.log('Task Manager App')
