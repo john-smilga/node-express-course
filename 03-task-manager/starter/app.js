ea47298fdbd56aab0a51138cbeb29b7055f78ab7
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-20 12:43:04 +0000

    5 Steps

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -7,10 +7,16 @@ app.get('/hello', (req, res) => {
 })
 
 
+// app.get('/api/v1/tasks')          -get all the tasks
+// app.post('/api/v1/tasks')         -create a new task
+// app.get('/api/v1/tasks/:id')      -get single task
+// app.patch('/api/v1/tasks/:id')    -update task
+// app.delete('/api/v1/tasks/:id')   -delete task
+
 
 
 const port = 3000;
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
 
-//console.log('Task Manager App')
+
