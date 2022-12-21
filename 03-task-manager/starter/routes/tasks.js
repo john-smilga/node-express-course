c14d3f1c747dce0251afed10e36b922f032fe89f
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-21 22:40:48 +0000

    Router

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {getAllTasks} = require('../controlers/tasks')
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controlers/tasks");
 
-router.route('/').get(getAllTasks)
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
-module.exports = router
+module.exports = router;
