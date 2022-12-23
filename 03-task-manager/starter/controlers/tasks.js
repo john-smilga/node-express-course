4a3b77f053e7e3f76175e15a82da8599ceaec9a5
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-23 19:25:01 +0000

    db

--- 03-task-manager/starter/controlers/tasks.js
+++ 03-task-manager/starter/controlers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  res.send("Create task");
+  res.json(req.body);
 };
 
 const getTask = (req, res) => {
-  res.send("Get single task");
+  res.json({id:req.params.id});
 };
 
 const updateTask = (req, res) => {
