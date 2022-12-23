41b78e76d36996545621c59f1189f98621fb98c3
Uriel Ofir <urielofir@gmail.com>
2022-12-23 19:42:27 +0200

    Update 04-express-app.js
    fixing file path

--- 02-express-tutorial/final/04-express-app.js
+++ 02-express-tutorial/final/04-express-app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+  res.sendFile(path.resolve(__dirname, '../navbar-app/index.html'))
 })
 
 app.all('*', (req, res) => {
