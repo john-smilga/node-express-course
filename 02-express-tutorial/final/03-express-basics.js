b23aad5ecf5c12f0d173b617d18b8d51e562ba01
Mohd Shadmaan <124558510+shaad00@users.noreply.github.com>
2023-06-06 14:07:06 +0530

    Update 03-express-basics.js
    Remove unnecessary spaces and add a new method to initialize our PORT.

--- 02-express-tutorial/final/03-express-basics.js
+++ 02-express-tutorial/final/03-express-basics.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = 5000
 
 app.get('/', (req, res) => {
   console.log('user hit the resource')
@@ -14,8 +15,8 @@ app.all('*', (req, res) => {
   res.status(404).send('<h1>resource not found</h1>')
 })
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
+app.listen(PORT, () => {
+  console.log(`SERVER IS UP AND RUNNING ON PORT ${PORT}`)
 })
 
 // app.get
