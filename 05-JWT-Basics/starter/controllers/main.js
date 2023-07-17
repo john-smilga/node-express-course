447c2cf0a9f1c6ebdcd372afab666618f0914aec
Daniel Home <hinilyu@gmail.com>
2023-07-17 12:05:20 -0400

    05-JWT-Basics

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,12 +1,23 @@
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+  // check in the controller
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
   res.send("Fake Login/Register/Signup Route");
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({msg:`Hello, Daniel`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
+  res.status(200).json({
+    msg: `Hello, Daniel`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
-    login, dashboard
-}
+  login,
+  dashboard,
+};
