e802bde7992da4b10bca466fda149de72ca301fe
Tenzing Sherpa <pasangsherpa689@gmail.com>
2023-12-14 09:10:06 +0800

    learnined http headers

---
+++ 01-node-tutorial/01-intro.js
@@ -0,0 +1,9 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log("small number");
+} else {
+  console.log("large number");
+}
+
+console.log(`hey it's my first node app!!!`);
--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,20 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url == "/contact") {
+    res.writeHead(400, { "content-type": "text/html" });
+    res.end("<h1> Page Not Found</h1>");
+  }
+  if (url == "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1> this is about page</h1>");
+  }
+
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>Welcome</h1>");
+  res.end();
+});
+
+server.listen(4000);
