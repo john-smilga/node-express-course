e8eac940f8eaef37cba425e6c6e5c2cbb8acb039
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-01-04 10:27:46 +0800

    queyr params advanced

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,18 @@
+const { query } = require("express");
 const Product = require("../models/product");
 
-const getAllProducts = async (req, res) =>
+const getAllProducts = async (req, res) => {
   res.status(200).json({ msg: "product testing routed" });
+};
 const getAllProductStatic = async (req, res) => {
-  const allStaticProducts = await Product.find();
-  if (!allStaticProducts) {
-    throw new Error("Error Happend");
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+    console.log(queryObject);
   }
+  const allStaticProducts = await Product.find(queryObject);
+
   res.status(200).json({ allStaticProducts, nbHits: allStaticProducts.length });
 };
 
