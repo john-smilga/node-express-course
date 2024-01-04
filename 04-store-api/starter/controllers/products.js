2cf0ebc7c2806b6914cfaf739a2f338f831f8e6a
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-01-04 11:11:47 +0800

    regex search query

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,18 +2,26 @@ const { query } = require("express");
 const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "product testing routed" });
-};
-const getAllProductStatic = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company } = req.query;
+  const testname = "ab";
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
     console.log(queryObject);
   }
-  const allStaticProducts = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+  // if (name) {
+  //   queryObject.name = name;
+  // }
 
-  res.status(200).json({ allStaticProducts, nbHits: allStaticProducts.length });
+  const products = await Product.find({ name: { $regex: testname } });
+  res.status(200).json({ products, nbHits: products.length });
+};
+const getAllProductStatic = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({ products });
 };
 
 module.exports = {
