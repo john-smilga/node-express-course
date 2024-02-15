a0dcaea030fb8580c8711333cd7354e8ea7675b5
K V VINITHA <kvvinithareddy@gmail.com>
2024-02-15 13:41:05 -0800

    Express JS

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,16 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	const method = req.method;
+
+	res.send("Home");
+});
+
+app.get("/abput", (req, res) => {
+	res.send("About");
+});
+
+app.listen(5000, () => {
+	console.log("server is listening to port");
+});
--- 02-express-tutorial/final/04-express-app.js
+++ 02-express-tutorial/final/04-express-app.js
@@ -1,19 +1,25 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
 
-const app = express()
+// to get absolute path of the file
+const path = require("path");
+
+const app = express();
 
 // setup static and middleware
-app.use(express.static('./public'))
+/*
+app.use() is used to set up middleware. express.static is a built in middleware.
+static assets are files that server doen't have to change , example  image files,CSS files
+*/
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+app.get("/", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+app.all("*", (req, res) => {
+	res.status(404).send("resource not found");
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+	console.log("server is listening on port 5000....");
+});
